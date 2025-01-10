3113975655b9bc5b3bc2aa041b4ae684b8dfc7e1
Alec Reimel <alecreimel1@gmail.com>
2025-01-10 14:46:34 -0500

    home major progress, footer progress

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,11 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    colors: {
+      'black': '#000000',
+      'white': '#ffffff',
+      "gold": "#FFD700",
+    },
     extend: {
       fontFamily: {
         sans: [
