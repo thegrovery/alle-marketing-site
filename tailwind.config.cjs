d76f6f9fb25cd6121b7fe7a28f04055821402570
Alec Reimel <alecreimel1@gmail.com>
2025-01-13 14:00:23 -0500

    home wrap-up pass pt1

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,16 +3,17 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    colors: {
-      'black': '#070113',
-      'black-1': '#0A0A0A',
-      'white': '#ffffff',
-      'white-1': '#C0C0C0',
-      "gold": "#EFBD42",
-      "green": "#4DD64B",
-      "green-1": "#79F0B0",
-    },
+    
     extend: {
+      colors: {
+        'black': '#070113',
+        'black-1': '#0A0A0A',
+        'white': '#ffffff',
+        'white-1': '#C0C0C0',
+        "gold": "#EFBD42",
+        "green": "#4DD64B",
+        "green-1": "#79F0B0",
+      },
       fontFamily: {
         sans: [
           "Bricolage Grotesque Variable",
