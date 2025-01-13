707244c43ef8c9e912e3bc0d2df3cdd771f23da6
Alec Reimel <alecreimel1@gmail.com>
2025-01-13 12:18:30 -0500

    header update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,9 +4,13 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     colors: {
-      'black': '#000000',
+      'black': '#070113',
+      'black-1': '#0A0A0A',
       'white': '#ffffff',
-      "gold": "#FFD700",
+      'white-1': '#C0C0C0',
+      "gold": "#EFBD42",
+      "green": "#4DD64B",
+      "green-1": "#79F0B0",
     },
     extend: {
       fontFamily: {
